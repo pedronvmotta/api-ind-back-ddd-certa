f0b5e26b919933283b62eeed88600e046465e1e5
pedronvmotta <pedronaves.20241@poli.ufrj.br>
2026-03-21 14:09:23 -0300

    terminando as dtos

--- src/item-emprestimo/dto/create-item-emprestimo.dto.ts
+++ src/item-emprestimo/dto/create-item-emprestimo.dto.ts
@@ -1,1 +1,40 @@
-export class CreateItemEmprestimoDto {}
+import { IsInt, IsString, IsDate } from "class-validator";
+import { ApiProperty} from "@nestjs/swagger";
+
+export class CreateItemEmprestimoDto {
+    @ApiProperty({
+        description: "id do empréstimo",
+        example: "id:1"
+    })
+    @IsInt()
+    id_emprestimo!: number
+
+    @ApiProperty({
+        description: "id do livro",
+        example:"id:1"
+    })
+    @IsInt()
+    id_livro!: number
+
+    @ApiProperty({
+        description: "data de devolucao",
+        example: 1/12/26
+    })
+    @IsDate()
+    data_de_devolucao!: Date;
+
+    @ApiProperty({
+        description: "valor da multa caso o livro seja devolvido atrasado",
+        example: 10.50
+    })
+    @IsInt()
+    valor_multa!: number;
+
+    @ApiProperty({
+        description: "status atual do empréstimo",
+        example: "terminado"
+    })
+    @IsString()
+    status !: string;
+
+}
--- src/livro/dto/create-livro.dto.ts
+++ src/livro/dto/create-livro.dto.ts
@@ -1,1 +1,74 @@
-export class CreateLivroDto {}
+import { IsInt, IsString, IsDate } from "class-validator";
+import { ApiProperty} from "@nestjs/swagger";
+
+export class CreateLivroDto {
+    @ApiProperty({
+        description: "id do livro",
+        example: "id : 1"
+    })
+    @IsInt()
+    id_livro!:number;
+
+    @ApiProperty({
+        description: "nome do livro",
+        example: "Rlk da barra da tijuca : volume 2"
+    })
+    @IsString()
+    nome_livro!:string;
+
+    @ApiProperty({
+        description: "editora do livro",
+        example: "aquela lá do pinguim"
+    })
+    @IsString()
+    editora!:string;
+
+    @ApiProperty({
+        description: "a versão do livro",
+        example: "14"
+    })
+    @IsString()
+    edicao!:number
+
+    @ApiProperty({
+        description: "o ano de lancamento do livro",
+        example: "2023"
+    })
+    @IsInt()
+    ano!:number
+
+    @ApiProperty({
+        description: "genero do livro",
+        example: "ação"
+    })
+    @IsString()
+    genero!:string
+
+    @ApiProperty({
+        description: "idioma",
+        example: "pt-br"
+    })
+    @IsString()
+    idioma!:string
+
+    @ApiProperty({
+        description: "quantidade total do livro (contando os não disponíveis no momento)",
+        example: "10"
+    })
+    @IsInt()
+    qtde_total!:number
+
+    @ApiProperty({
+        description: "quantidade disponivel no momento",
+        example: "5"
+    })
+    @IsInt()
+    qtde_disponivel!:number
+
+    @ApiProperty({
+        description: "id do autor",
+        example: "id:1"
+    })
+    @IsInt()
+    id_autor!:number
+}
