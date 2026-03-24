08b6fe80680cb61a32ef6695b5fe53d323a76d68
pedronvmotta <pedronaves.20241@poli.ufrj.br>
2026-03-24 16:48:34 -0300

    autor ok

--- src/autor/autor.controller.ts
+++ src/autor/autor.controller.ts
@@ -6,7 +6,53 @@ import { CreateAutorDto } from './dto/create-autor.dto';
 
 @Controller('autores')
 export class AutorController {
-  constructor(private readonly autorService: AutorService) {} // ← constructor vazio
+  constructor(private readonly autorService: AutorService) {} 
 
+@Post('/cadastro')
+@ApiOperation({summary: "cria um autor"})
+@ApiResponse({status:201, description: "autor criado"})
+@ApiResponse({status:400, description: 'autor não criado'})
+cadastrarAutor(@Body () createAutorDto: CreateAutorDto){
+  return this.autorService.cadastrarAutor(createAutorDto)
+}
+
+@Get('/listar-todos')
+@ApiOperation({summary:"lista todos os autores"})
+@ApiResponse({status:200, description: "autor encontrado"})
+@ApiResponse({status:404, description: 'autores não encontrados'})
+
+listarAutores(){ 
+  return this.autorService.listarAutores()
+}
+
+@Get('/pegar-dados-autor/:id')
+@ApiOperation({summary:"pega os dados de um autor"})
+@ApiResponse({status:200, description: "autor encontrado"})
+@ApiResponse({status:404, description: 'autor não encontrado'})
+@ApiParam({name: "id", type:Number})
+
+getDadosByAutor(@Param('id') id:number){
+  return this.autorService.getDadosByAutor(+id)
+}
+
+@Patch('/atualizar-dados/:id')
+@ApiOperation({summary:"atualiza os dados de um autor"})
+@ApiResponse({status:200, description: "autor atualizado"})
+@ApiResponse({status:404, description: 'autor não encontrado'})
+@ApiParam({name: "id", type:Number})
+
+updateDadosAutor(@Param('id') id:number, @Body() updateAutorDto: CreateAutorDto){
+  return this.autorService.updateDadosAutor(+id, updateAutorDto)
+}
+
+@Delete('/deletar-autor/:id')
+@ApiOperation({summary:"deleta um autor"})
+@ApiResponse({status:200, description: "autor deletado"})
+@ApiResponse({status:404, description: 'autor não encontrado'})
+@ApiParam({name: "id", type:Number})
+
+deletarAutor(@Param('id') id:number){
+  return this.autorService.deletarAutor(+id)
+}
   
 }
