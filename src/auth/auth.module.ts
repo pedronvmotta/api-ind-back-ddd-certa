0598c55233eef45bf8c5a2e476b37ec9ed00801d
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-03-24 16:22:18 -0300

    commitando

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
+import { LocalStrategy } from './strategies/local-strategy';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService, PrismaService, UserService],
+  providers: [AuthService, PrismaService, UserService, LocalStrategy, JwtService],
   controllers: [AuthController]
 })
 export class AuthModule {
