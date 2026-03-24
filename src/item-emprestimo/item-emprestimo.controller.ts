5b8e8fe0882c234a6e9f5a9f3eef8c98ecde9186
pedronvmotta <pedronaves.20241@poli.ufrj.br>
2026-03-24 17:08:52 -0300

    itemEmprestimo ok

--- src/item-emprestimo/item-emprestimo.controller.ts
+++ src/item-emprestimo/item-emprestimo.controller.ts
@@ -2,9 +2,59 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ItemEmprestimoService } from './item-emprestimo.service';
 import { CreateItemEmprestimoDto } from './dto/create-item-emprestimo.dto';
 import { UpdateItemEmprestimoDto } from './dto/update-item-emprestimo.dto';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
-@Controller('item-emprestimo')
+@Controller('itens-emprestimo')
 export class ItemEmprestimoController {
   constructor(private readonly itemEmprestimoService: ItemEmprestimoService) {}
   
+  @Post('/adicionar-item')
+  @ApiOperation({summary: "adiciona um item"})
+  @ApiResponse({status:201, description: "item criado"})
+  @ApiResponse({status:400, description: "item não criado"})
+
+  adicionarItemEmprestimo(createItemEmprestimoDto: CreateItemEmprestimoDto){
+    return this.itemEmprestimoService.adicionarItemEmprestimo(createItemEmprestimoDto)
+  }
+
+  @Get('/listar-todos-os-itens')
+  @ApiOperation({summary: "lista todos os empréstimos"})
+  @ApiResponse({status:200, description:"item found"})
+  @ApiResponse({status:404, description: "item not found"})
+
+  listarItensEmprestimo(){
+    return this.itemEmprestimoService.listarItensEmprestimo()
+  }
+
+  @Get('/pegar-item-especifico/:id')
+  @ApiOperation({summary: "pega os dados de um item de empréstimo específico"})
+  @ApiResponse({status:200, description:"item found"})
+  @ApiResponse({status:404, description: "item not found"})
+  @ApiParam({name:"id", type:Number})
+
+  verItemEspecificoEmprestimo(@Param('id') id:number){
+    return this.itemEmprestimoService.verItemEspecificoEmprestimo(+id)
+  }
+
+  @Patch('/atualizar-dados-item-emprestimo/:id')
+  @ApiOperation({summary: "atualiza os dados de um item de empréstimo específico"})
+  @ApiResponse({status:200, description:"item found"})
+  @ApiResponse({status:404, description: "item not found"})
+  @ApiParam({name:"id", type:Number})
+
+  atualizarDadosItem(@Param('id') id:number, @Body() updateItemEmprestimoDto: UpdateItemEmprestimoDto){
+    return this.itemEmprestimoService.atualizarDadosItem(+id, updateItemEmprestimoDto)
+  }
+
+  @Delete('/remover-item-de-emprestimo/:id')
+  @ApiOperation({summary: "deleta um item de empréstimo específico"})
+  @ApiResponse({status:200, description:"item found"})
+  @ApiResponse({status:404, description: "item not found"})
+  @ApiParam({name:"id", type:Number})
+
+  removerItemEmprestimo(@Param('id') id:number){
+    return this.itemEmprestimoService.removerItemEmprestimo(+id)
+  }
+
+
 }
